7762b1af45132d977a201c73dcba7b51623f1d17
ClementCaillaud <34067765+ClementCaillaud@users.noreply.github.com>
2017-12-12 16:19:47 +0100

    Exercise 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,18 @@
-exports.run = function(input) {
+exports.run = function(input)
+{
   console.log("Exercise #3");
+  var result = 0;
+
+  for(var i=0; i<input.length; i++)
+  {
+    if(input[i].operator == 'add')
+    {
+      result += input[i].value;
+    }
+    else if(input[i].operator == 'substract')
+    {
+      result -= input[i].value;
+    }
+  }
+  return result;
 };
