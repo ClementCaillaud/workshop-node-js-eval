1156af646d1e12e63a097c4a72b45c978a5198e8
ClementCaillaud <34067765+ClementCaillaud@users.noreply.github.com>
2017-12-12 16:43:08 +0100

    Exercise 5

--- src/exercise5.js
+++ src/exercise5.js
@@ -1,3 +1,17 @@
-exports.run = function(input) {
+exports.run = function(input)
+{
   console.log("Exercise #5");
+
+  var fs = require('fs');
+  var buf = fs.readFileSync(input);
+  var str = buf.toString();
+  var tab = str.split('\r\n');
+
+  var result = 0;
+
+  for(var i=0; i<tab.length; i++)
+  {
+    result += Number(tab[i]);
+  }
+  return result;
 };
