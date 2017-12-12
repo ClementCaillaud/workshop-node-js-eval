3788ee92e2894b55ea9eb7dbd60cdd79f20e278e
ClementCaillaud <34067765+ClementCaillaud@users.noreply.github.com>
2017-12-12 16:07:26 +0100

    Exercise 2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,3 +1,13 @@
-exports.run = function(input) {
+exports.run = function(input)
+{
   console.log("Exercise #2");
+  var output = "";
+  for (var i=0; i<input.length; i++)
+  {
+    if(input[i]<6)
+    {
+      output += input[i];
+    }
+  }
+  return output;
 };
