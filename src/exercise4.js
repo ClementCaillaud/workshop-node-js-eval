52c8081a84fb604cdf03bf7cacf2c61595be0c62
ClementCaillaud <34067765+ClementCaillaud@users.noreply.github.com>
2017-12-12 16:33:06 +0100

    Exercise 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,9 +1,25 @@
-exports.run = function(input) {
+exports.run = function(input)
+{
   console.log("Exercise #4");
 
   // YOUR CODE IS WELCOME HERE
+
 
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(myFunction, [1, 2, 3], ["One", "Two", "Three"]);
   /** */
 };
+
+function myFunction(numbers, letters)
+{
+  var result = "";
+  for(var i=0; i<numbers.length; i++)
+  {
+    result += "The value of "+letters[i]+" is "+numbers[i];
+    if(letters[i+1])
+    {
+      result += ", ";
+    }
+  }
+  return result;
+}
